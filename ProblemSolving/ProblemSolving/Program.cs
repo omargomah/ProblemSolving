668c8e65ce390773c36565f02e9a52349d683430
Omar Gomaa <mrjmh934@gmail.com>
2026-02-08 00:08:03 +0200

    Problem solving

--- ProblemSolving/ProblemSolving/Program.cs
+++ ProblemSolving/ProblemSolving/Program.cs
@@ -4,11 +4,6 @@
     {
         static void Main(string[] args)
         {
-            var result = SolutionArrayAndHashing.ProductExceptSelf([-1, 0, 1, 2, 3]);
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
         }
         #region Array & Hashing
 
@@ -162,27 +157,68 @@
 
             #region Valid Sudoku
 
-            //public static bool IsValidSudoku(char[][] board)
-            //{
+            public static bool IsValidSudoku(char[][] board)
+            {
+                char x,y;
+                for (int i = 0; i < 9; i++)
+                {
+                    HashSet<char> mapRow =  new HashSet<char>();
+                    HashSet<char> mapColumn =  new HashSet<char>();
+                    for (int j = 0; j < 9; j++)
+                    {
+                        x = board[i][j];
+                        y = board[j][i];
+                        if (x != '.')
+                        {
+                            if (mapRow.Contains(x))
+                                return false;
+                            mapRow.Add(x);
+                        }
 
-            //    int[,] mapRow = new int[9, 10];
-            //    int[,] mapColumn = new int[9, 10];
-            //    int[,] mapSquare = new int[9, 10];
-            //    char x;
-            //    for (int i = 0; i < 9; i++)
-            //    {
-            //        for (int j = 0; j < 9; j++)
-            //        {
-            //            x = board[i][j];
-            //            if (x != '.')
-            //            {
-            //                mapRow[i, x - '0']++;
-            //                mapColumn[j, x - '0']++;
-            //                mapSquare[,]
-            //            }
-            //        }
-            //    }
-            //}
+                        if (y != '.')
+                        {
+                            if (mapColumn.Contains(y))
+                                return false;
+                            mapColumn.Add(y);
+                        }
+
+                    }
+                }
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        HashSet<char> checkSet =  new HashSet<char>();
+                        for (int h = i*3; h < (i * 3 + 3) ; h++)
+                        {
+                            for(int k = j*3; k < (j * 3 + 3); k++)
+                            {
+                                x = board[h][k];
+                                if (x != '.')
+                                {
+                                    if (checkSet.Contains(x))
+                                        return false;
+                                    checkSet.Add(x);
+                                }
+                            }
+
+                        }
+                    }
+                }
+                return true;
+
+            }
+
+            #endregion
+
+
+            #region Longest Consecutive Sequence
+
+            public static int LongestConsecutive(int[] nums)
+            {
+
+            }
+
             #endregion
 
         }
--- ProblemSolving/ProblemSolving/obj/Debug/net8.0/ProblemSolving.AssemblyInfo.cs
+++ ProblemSolving/ProblemSolving/obj/Debug/net8.0/ProblemSolving.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProblemSolving")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e39891e8f1c5c8915f14c1e0a304bedacb2b1da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fd89b8d3f9d0ed247bdee93bb8430e5cf48eef5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProblemSolving")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProblemSolving")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
