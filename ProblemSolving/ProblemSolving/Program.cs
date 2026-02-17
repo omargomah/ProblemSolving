2f92c657c119a350bcec0953001fe87063ac736b
Omar Gomaa <mrjmh934@gmail.com>
2026-02-17 23:24:21 +0200

    prblem solving

--- ProblemSolving/ProblemSolving/Program.cs
+++ ProblemSolving/ProblemSolving/Program.cs
@@ -557,6 +557,34 @@ namespace ProblemSolving
             }
             #endregion
 
+            #region Car Fleet
+            public static int CarFleet(int target, int[] position, int[] speed)
+            {
+                List<(int,int)> list = new List<(int, int)>();
+                for (int i = 0; i < position.Length; i++)
+                    list.Add((position[i], speed[i]));
+                var sortedList = list.OrderBy(x => x.Item1).ToList();
+                Stack<double> s = new Stack<double>();
+                double time;
+                for (int i = sortedList.Count - 1; i > -1; i--)
+                {
+                    time = (target - sortedList[i].Item1) / (double)sortedList[i].Item2;
+                    if (s.Count == 0 || time > s.Peek())
+                        s.Push(time);
+                }
+                return s.Count;
+            }
+
+            #endregion
+
+            #region Largest Rectangle In Histogram
+            public static int LargestRectangleArea(int[] heights)
+            {
+
+
+
+            }
+            #endregion
         }
 
         #endregion
